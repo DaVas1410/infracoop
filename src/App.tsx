fd624421fa61dbb93d1f65c063385503ab2d2f70
davas <sedigraph7@gmail.com>
2026-04-26 11:14:54 -0500

    feat: add SearchIndexProvider that preloads MiniSearch index at app startup

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import { Landing } from './pages/Landing'
 import { MonitorBrechas } from './pages/MonitorBrechas'
 import { MonitorColectivo } from './pages/MonitorColectivo'
 import { DatosQueremos } from './pages/DatosQueremos'
+import { SearchIndexProvider } from './context/SearchIndexContext'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/brechas" element={<MonitorBrechas />} />
-        <Route path="/colectivo" element={<MonitorColectivo />} />
-        <Route path="/datos" element={<DatosQueremos />} />
-      </Routes>
-    </BrowserRouter>
+    <SearchIndexProvider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/brechas" element={<MonitorBrechas />} />
+          <Route path="/colectivo" element={<MonitorColectivo />} />
+          <Route path="/datos" element={<DatosQueremos />} />
+        </Routes>
+      </BrowserRouter>
+    </SearchIndexProvider>
   )
 }
