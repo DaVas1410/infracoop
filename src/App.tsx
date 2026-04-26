e2d8f126aa0a0376a6339712ea98602e64ade8df
davas <sedigraph7@gmail.com>
2026-04-26 18:19:02 -0500

    feat: add /ingresar route and document VITE_REQUIRE_SUPERVISION

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Landing } from './pages/Landing'
 import { MonitorBrechas } from './pages/MonitorBrechas'
 import { MonitorColectivo } from './pages/MonitorColectivo'
 import { DatosQueremos } from './pages/DatosQueremos'
+import { IngresoForm } from './pages/IngresoForm'
 import { SearchIndexProvider } from './context/SearchIndexContext'
 
 export default function App() {
@@ -16,6 +17,7 @@ export default function App() {
           <Route path="/brechas" element={<MonitorBrechas />} />
           <Route path="/colectivo" element={<MonitorColectivo />} />
           <Route path="/datos" element={<DatosQueremos />} />
+          <Route path="/ingresar" element={<IngresoForm />} />
         </Routes>
       </BrowserRouter>
     </SearchIndexProvider>
